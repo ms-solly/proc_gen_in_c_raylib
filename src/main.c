78c0df0fb2920330bdd05a7a1b78bf30c87539f9
ms-solly <theworldofthoughtsyo@gmail.com>
2025-09-08 19:29:16 +0530

    player added(incomplete)

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-#include "raylib.h"
+#include <raylib.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
@@ -6,7 +6,7 @@
 #include<inttypes.h>
 
 #define FNL_IMPL
-#include "../res/FastNoiseLite/C/FastNoiseLite.h"
+#include "../res/include/FastNoiseLite/C/FastNoiseLite.h"
 //#define FASTNOISELITE_H
 
 #define SCREEN_WIDTH 1200
@@ -19,11 +19,85 @@ typedef enum {
 	TILE_TYPE_WATER,
 } TyleType;
 
+typedef enum{
+	TILE_BASE_DIRT,
+	TILE_BASE_WATER,
+}TileBase;
+
+typedef enum{
+	TILE_FEATURE_NONE,
+	TILE_FEATURE_GRASS
+}TileFeature;
+
+typedef struct{
+	TileBase base;
+	TileFeature feature;
+}Tile;
+
+typedef enum{
+	DIRECTION_LEFT,
+	DIRECTION_RIGHT,
+	DIRECTION_UP,
+	DIRECTION_DOWN
+}Direction;
+
+
+typedef struct{
+	Vector2 position;
+	float speed;
+	Texture texture;
+}Player;
+
 #define WORLD_WIDTH  SCREEN_WIDTH 
 #define WORLD_HEIGHT SCREEN_HEIGHT
 
 TyleType world_data[WORLD_HEIGHT][WORLD_WIDTH];
 
+Tile world_tiles[WORLD_HEIGHT][WORLD_WIDTH];
+
+/*
+* static void generate_world(void){
+	//unsigned int seed = 90000;
+	//srand(seed); // seed
+
+	//FastNoiseLite noise;
+	fnl_state noise = fnlCreateState();
+	noise.noise_type = FNL_NOISE_OPENSIMPLEX2;
+	noise.seed = (uint32_t)time(NULL);
+	//noise.frequency = 0.03f;
+
+	fnl_state feature_noise = fnlCreateState();
+	feature_noise.seed = 5678;
+
+
+	float scale = 8.897f;
+	float min_mapped = 1.0f, max_mapped = 0.0f;
+	int count_grass = 0, count_dirt = 0, count_water = 0;
+ 
+
+	//float noise_val;
+	for(uint32_t y = 0; y < WORLD_HEIGHT; y++){
+		for(uint32_t x = 0; x < WORLD_WIDTH; x++){
+			//uint random_val = rand() % 100;
+			
+			float raw = fnlGetNoise2D(&noise, (float)x * scale, (float)y * scale);
+			float n = (raw + 1.0f) * 0.5f;
+			if (n < min_mapped) min_mapped = n;
+			if (n > max_mapped) max_mapped = n;
+		
+			if(n < 0.40f ) {
+				world_data[y][x] = TILE_TYPE_GRASS;
+				count_grass++;
+			} else if (n < 0.60f) {				world_data[y][x] = TILE_TYPE_DIRT;
+				count_dirt++;
+			}else{
+				world_data[y][x] = TILE_TYPE_WATER;
+				count_water++;
+			}
+		}
+	}
+}
+*/ 
 static void generate_world(void){
 	//unsigned int seed = 90000;
 	//srand(seed); // seed
@@ -33,8 +107,12 @@ static void generate_world(void){
 	noise.noise_type = FNL_NOISE_OPENSIMPLEX2;
 	noise.seed = (uint32_t)time(NULL);
 	//noise.frequency = 0.03f;
-	
-	float scale = 3.897f;
+
+	fnl_state feature_noise = fnlCreateState();
+	feature_noise.seed = 5678;
+
+
+	float scale = 8.897f;
 	float min_mapped = 1.0f, max_mapped = 0.0f;
 	int count_grass = 0, count_dirt = 0, count_water = 0;
  
@@ -62,12 +140,57 @@ static void generate_world(void){
 	}
 }
 
+static Player* create_player(Player *player, Vector2 position, float speed, Texture texture){
+	player->position = position;
+	player->speed = speed;
+	player->texture = texture;
+
+	return player;
+}
+
+
+
+
 int main() {
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "C raylib template");
 	SetTargetFPS(60);
 
-generate_world();
+	Player player;
+	player.texture = LoadTexture("res/art/sheep.png");
+	player.position = (Vector2){20,15};
+	generate_world();
+	create_player(&player, player.position, 4.0f, player.texture);
 	while (!WindowShouldClose()){
+
+		int next_x = player.position.x;
+		int next_y = player.position.y;
+
+		if (IsKeyPressed(KEY_RIGHT)) {
+			next_x++;
+		}
+		if (IsKeyPressed(KEY_LEFT)) {
+			next_x--;
+		}
+		if (IsKeyPressed(KEY_UP)) {
+			next_y--;
+		}
+		if (IsKeyPressed(KEY_DOWN)) {
+			next_y++;
+		}
+
+		if (next_x >= 0 && next_x < WORLD_WIDTH && next_y >= 0 && next_y < WORLD_HEIGHT) {
+
+			// Get the type of tile at our target destination
+			TyleType target_tile = world_data[next_y][next_x];
+			// THIS IS THE ANSWER TO YOUR QUESTION:
+			// We check if the target tile is walkable
+			if (target_tile == TILE_TYPE_GRASS || target_tile == TILE_TYPE_DIRT) {
+				// 3. If it's valid, update the player's actual position 
+				player.position.x = next_x;
+				player.position.y = next_y;
+			}
+        // If the tile was WATER, we do nothing! The player doesn't move.
+   }
 		BeginDrawing();
 		//ClearBackground(GREEN);
 				Color tile_color;
@@ -75,8 +198,8 @@ generate_world();
 
 		int offset_x = (SCREEN_WIDTH  - (WORLD_WIDTH  * TILE_SIZE)) / 2;
 		int offset_y = (SCREEN_HEIGHT - (WORLD_HEIGHT * TILE_SIZE)) / 2;
-		for(uint32_t y = 0; y < WORLD_HEIGHT; y++){
-			for(uint32_t x = 0; x < WORLD_WIDTH; x++){
+		for(unsigned long int y = 0; y < WORLD_HEIGHT; y++){
+			for(unsigned long int x = 0; x < WORLD_WIDTH; x++){
 
 				switch(world_data[y][x]){
 					case TILE_TYPE_GRASS: tile_color = GREEN;
@@ -89,15 +212,18 @@ generate_world();
 				DrawRectangle(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, tile_color);
 			}
 		}
+		
+				DrawRectangle(player.position.x * TILE_SIZE, player.position.y *
+     TILE_SIZE, TILE_SIZE, TILE_SIZE, RED);
 
-
-		//DrawText("C Template", GetScreenWidth()/2 - MeasureText("C Template",100), GetScreenHeight()/2, 100, WHITE);
-
-
-        EndDrawing();
-    }
+	Rectangle src = { 0, 0, 300, 300 }; 
+	Rectangle dest = { player.position.x * TILE_SIZE, player.position.y * TILE_SIZE, TILE_SIZE, TILE_SIZE };
+	DrawTexturePro(player.texture, src, dest, (Vector2){0, 0}, 0, WHITE);
     
 
-    CloseWindow();
-    return 0;
+		EndDrawing();
+	}
+	UnloadTexture(player.texture);
+	CloseWindow();
+	return 0;
 }
